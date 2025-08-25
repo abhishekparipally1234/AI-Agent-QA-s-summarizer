e95694cd2922699f02f940c8249a5cbd5e8ef7c3
Paripally Abhishek <161805879+abhishekparipally1234@users.noreply.github.com>
2025-08-25 17:52:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+(function(){
+  if(!window.chatbase || window.chatbase("getState") !== "initialized") {
+    window.chatbase = (...arguments) => {
+      if(!window.chatbase.q) { window.chatbase.q = [] }
+      window.chatbase.q.push(arguments)
+    };
+    window.chatbase = new Proxy(window.chatbase, {
+      get(target, prop) {
+        if(prop === "q") { return target.q }
+        return (...args) => target(prop, ...args)
+      }
+    });
+  }
+
+  const onLoad = function() {
+    const script = document.createElement("script");
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.id = "FbJspZRylRz7AopmrdvuP"; // your Chatbase bot ID
+    script.domain = "www.chatbase.co";
+    document.querySelector(".chat-container").appendChild(script);
+  };
+
+  if(document.readyState === "complete") { onLoad() }
+  else { window.addEventListener("load", onLoad) }
+})();
